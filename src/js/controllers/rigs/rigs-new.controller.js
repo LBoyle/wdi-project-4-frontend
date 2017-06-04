66877796ed560ebbd774c49766e1b36b3acd5114
LBoyle <louis_boyle@hotmail.com>
2017-06-04 22:16:36 +0100

    progress on the rigs new page

--- src/js/controllers/rigs/rigs-new.controller.js
+++ src/js/controllers/rigs/rigs-new.controller.js
@@ -2,13 +2,37 @@ angular
   .module('pcBuilderApp')
   .controller('RigsNewCtrl', RigsNewCtrl);
 
-RigsNewCtrl.$inject = ['Rig'];
-function RigsNewCtrl(Rig) {
+RigsNewCtrl.$inject = ['Rig', 'Part', 'Parttype', 'filterFilter', 'TokenService'];
+function RigsNewCtrl(Rig, Part, Parttype, filterFilter, TokenService) {
   const vm = this;
+  // console.log(vm.newRig());
 
-  vm.newRig = {
-    description: '',
-    user: '',
-    parts: []
+  vm.description = '';
+  vm.partNames = [];
+  vm.partIds = [];
+  
+  vm.newRig = () => {
+    return {
+      description: vm.description,
+      user_id: TokenService.decodeToken().id,
+      parts: vm.partIds
+    };
   };
+
+  Part.query().$promise
+  .then(res => {
+    vm.partsAll = res;
+  });
+
+  vm.submitParts = () => {
+    vm.partNames.forEach(part => {
+      vm.partIds.push(filterFilter(vm.partsAll, {name: part})[0].id);
+      console.log(vm.newRig());
+    });
+  };
+
+  Parttype.query().$promise
+  .then(types => {
+    vm.types = types;
+  });
 }
---
+++ src/js/factories/parttype.factory.js
@@ -0,0 +1,12 @@
+angular
+  .module('pcBuilderApp')
+  .factory('Parttype', Parttype);
+
+Parttype.$inject = ['API', '$resource'];
+function Parttype(API, $resource) {
+  return $resource(`${API}/parttypes/:id`, {id: '@_id'},
+    {
+      'update': { method: 'PUT' }
+    }
+  );
+}
