3a275049c725fea361c352fbf7d0db75831bfc47
LBoyle <louis_boyle@hotmail.com>
2017-06-07 00:41:19 +0100

    got ng-options working sort of for a while, broke it

--- src/js/controllers/rigs/rigs-edit.controller.js
+++ src/js/controllers/rigs/rigs-edit.controller.js
@@ -19,44 +19,41 @@ function RigsEditCtrl(
   const vm = this;
 
   vm.rigPartsByType = [];
+  vm.newParts = [];
 
-  vm.getPartsInCategory = (part) => {
-    const type = part.parttypes[0].parttype;
-    return filterFilter(vm.types, {parttype: type})[0].parts.filter(p => p.name !== part.name);
-  };
-
-  vm.getRigPartsByCategory = () => {
-    setTimeout(() => {
-      vm.rig.parts.forEach(part => {
-        vm.rigPartsByType.push({
-          part: part,
-          type: part.parttypes[0].parttype
-        });
+  Rig.get($stateParams).$promise
+  .then(rig => {
+    vm.rig = rig;
+    rig.parts.forEach(part => {
+      vm.rigPartsByType.push({
+        part: part,
+        type: part.parttypes[0].parttype
       });
-    }, 500);
+      // vm.newParts.push(part.id);
+    });
+    return Parttype.query();
+  })
+  .then(types => {
+    vm.types = types;
+  });
+
+
+  // vm.getPartsInCategory = (part) => {
+  //   const type = part.parttypes[0].parttype;
+  //   console.log(filterFilter(vm.types, {parttype: type})[0].parts);
+  //   return filterFilter(vm.types, {parttype: type})[0].parts;
+  //   // .filter(p => p.name !== part.name)
+  // };
+
+  vm.getPartsInCategoryx = (part) => {
+    return vm.types[part.parttypes[0].parttype].parts;
   };
 
   vm.filterRigPartsByCategory = (cat) => {
     return filterFilter(vm.rigPartsByType, {type: cat})[0].part;
   };
 
-  Part.query().$promise
-  .then(res => {
-    vm.parts = res;
-    // console.log(vm.parts);
-  });
-
-  Rig.get($stateParams).$promise
-  .then(rig => {
-    vm.rig = rig;
-    // console.log(vm.rig);
-  });
-
-  Parttype.query().$promise
-  .then(types => {
-    vm.types = types;
-  });
-
-  vm.getRigPartsByCategory();
-  // vm.filterRigPartsByCategory('drive');
+  vm.updateRig = () => {
+    console.log(vm.newParts.filter(n => n != false));
+  };
 }
